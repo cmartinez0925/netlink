d2c7c7b7eef2570e5416ee99fce2082c00cf966e
cmartinez0925 <christopher.martinez0925@live.com>
2026-06-03 00:28:05 -0500

    WIP

--- netlink/modules/dns_spoof/__inti__.py
+++ netlink/modules/dns_spoof/__inti__.py
@@ -23,6 +23,18 @@ Description:
 """
 
 import argparse
+import ipaddress
+import itertools
+import signal
+
+from functools import partial
+from scapy.all import sniff, send
+from scapy.layers.dns import DNS, DNSQR, DNSRR
+from scapy.layers.inet import IP, UDP
+from scapy.layers.l2 import Ether
+from scapy.packet import Packet
+from typing import Any
+from types import FrameType
 
 from netlink.core.base_module import BaseModule
 from netlink.core.output import OutputManager
@@ -32,31 +44,70 @@ class DNSSpoof(BaseModule):
 
     """
 
-    ####################################################################
+    ############################################################################
     # Class Level Attributes
-    ####################################################################
+    ############################################################################
     NAME: str = "dns_spoof"
-    DESCRIPTION: str = ""
+    DESCRIPTION: str = "Performs DNS spoofing on a designated victim"
     REQUIRES_ROOT: bool = True
 
-    ####################################################################
+    DNS_FILTER = "udp and port 53"
+
+    ############################################################################
     # Constructor
-    ####################################################################
+    ############################################################################
     def __init__(self, iface: str, output: OutputManager):
         """
 
         """
         super().__init__(iface, output)
-       
+        self._keyboard_interrupted = False
 
-    ####################################################################
+    ############################################################################
     # Abstract Required Methods
-    ####################################################################
+    ############################################################################
     def add_args(self, parser: argparse.ArgumentParser) -> None:
         """
 
         """
-        pass
+        parser.add_argument(
+            '-t',
+            '--target',
+            type=str,
+            action='store',
+            dest='target',
+            required=True,
+            help="The target's IP address where the DNS Response will be sent"
+        )
+
+        parser.add_argument(
+            '-s',
+            '--spoof-ip',
+            type=str,
+            action='store',
+            dest='spoof_ip',
+            required=True,
+            help="The f"
+        )
+
+        parser.add_argument(
+            '-d',
+            '--domain',
+            type=str,
+            action='store',
+            dest='domain',
+            required=True,
+            help="The domain address"
+        )
+
+        parser.add_argument(
+            '--ttl',
+            type=int,
+            action='store',
+            dest='ttl',
+            default=300,
+            help=""
+        )
 
     def run(self, args: argparse.Namespace) -> None:
         """
@@ -64,11 +115,23 @@ class DNSSpoof(BaseModule):
         """
         pass
 
-    ####################################################################
+    ############################################################################
     # Methods
-    ####################################################################
+    ############################################################################
     def validate_args(self, args: argparse.Namespace) -> bool:
         """
 
         """
         return True
+
+    def _process_packet(self, pkt: Packet, args: argparse.Namespace) -> None:
+        """
+        
+        """
+        pass
+
+    def _signint_handler(self, sig: int, frame: FrameType|None) -> None:
+        """
+        
+        """
+        self._keyboard_interrupted = True
